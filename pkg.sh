e64ae9801756c1d9b4ded06cdb26eacb24c9620c
Ohio2 <sekmartyna4@gmail.com>
2020-10-28 14:12:52 +0100

    spelling error fix

--- pkg.sh
+++ pkg.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 zip -q Scratch-Edge.zip *
 mv Scratch-Edge.zip Scratch-Edge.sb3
-echo -e '\e[36mPackaged to and ready to be inputed to \e[93mScratch'
+echo -e '\e[36mPackaged and ready to be inputed to \e[93mScratch'
