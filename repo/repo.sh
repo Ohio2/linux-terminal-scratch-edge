4ce09af4cc9f88f928e0e8d1f396c29ef4490b17
Ohio2 <sekmartyna4@gmail.com>
2020-10-28 17:43:03 +0100

    added an another sh script

---
+++ repo/repo.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+echo -e '\e[94mRepo script.'
+echo -e '\e[39m1. Patch \e[91m(requires pkg.sh )\e[39m'
+for (( i=5; i>0; i--)); do
+  sleep 1 &
+  printf "  $i \r"
+  wait
+done
+set input=1 #should be one
+if [ input=1 ] #should be one
+then 
+ mkdir root_repo/
+ cd .. 
+ cp *(repo) repo/root
+ cd repo/
+ echo -e '\e[96mCopied!'
+ cd root_repo/
+else
+ echo -e '\e[91mError! Something broke! Check the script for more info'
+fi 
+nano pkg.sh
---
+++ repo/root_repo/README.md
@@ -0,0 +1,5 @@
+# linux-terminal-scratch-edge
+## to compile:
+make sure you have ```zip``` installed 
+
+```sh pkg.sh```
