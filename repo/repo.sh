5ae6740828c28d47d150cbcf361a63c2b997dbc3
Ohio2 <sekmartyna4@gmail.com>
2020-10-31 15:38:47 +0100

    Beta 0.41.0-pre4

--- repo/repo.sh
+++ repo/repo.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+git pull 2>/dev/null
 echo -e '\e[94mRepo script.'
 echo -e '\e[39m1. Patch \e[91m(requires pkg.sh )\e[39m'
 for (( i=5; i>0; i--)); do
@@ -9,9 +10,9 @@ done
 set input=1 #should be one
 if [ input=1 ] #should be one
 then 
- mkdir root_repo/
+ mkdir root_repo/ 2>/dev/null
  cd .. 
- cp * repo/root_repo
+ cp * repo/root_repo 2>/dev/null
  cd repo/
  echo -e '\e[96mCopied!'
  cd root_repo/
@@ -19,11 +20,13 @@ else
  echo -e '\e[91mError! Something broke! Check the script for more info'
 fi 
 echo '#Edit here! No patch guides yet...' >> pkg.sh
-nano pkg.sh
+echo 'echo '' ' >> pkg.sh
+vim pkg.sh
 echo -e '\e[94mPassing on to pkg.sh (modded, in repo)'
 sh pkg.sh 
 mv Scratch-Edge.sb3 .. 
 cd .. 
 rm -rf root_repo
+
 
 
