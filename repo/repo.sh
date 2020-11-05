647f165e5d2ca644c580722403b73d53c3dd538b
Ohio2 <sekmartyna4@gmail.com>
2020-11-05 15:09:45 +0100

    Changed some things, and tested out GitG
    Signed-off-by: Ohio2 <sekmartyna4@gmail.com>

--- repo/repo.sh
+++ repo/repo.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
+ping github.com -c 1 >/dev/null
+echo -e '\e[92mPinged GitHub.com'
 git pull 2>/dev/null
 echo -e '\e[94mRepo script.'
 echo -e '\e[39m1. Patch \e[91m(requires pkg.sh )\e[39m'
 for (( i=5; i>0; i--)); do
   sleep 1 &
-  printf "  $i \r"
+  printf "$i \r"
   wait
 done
 set input=1 #should be one
@@ -17,17 +19,21 @@ then
  echo -e '\e[96mCopied!'
  cd root_repo/
 else
- echo -e '\e[91mError! Something broke! Check the script for more info'
-fi 
+ echo -e '\e[91mError! Something broke! Check the script for more info, aditionally you could report this to my GitHub'
+fi
 echo '#Edit here! No patch guides yet...' >> pkg.sh
 # BO Editors
 # NANO
+# set val.editor.list=1
 # nano pkg.sh
 # VIM (DEFAULT), DISABLED
-#vim pkg.sh
+# set val.editor.list=2
+# vim pkg.sh
 # NVIM
+# set val.editor.list=3
 # nvim pkg.sh
 # EMACS
+# set val.editor.list=4
 # emacs pkg.sh
 # EO Editors
 echo -e '\e[94mPassing on to pkg.sh (modded, in repo)'
