5bd71c4f6e30257bcf877107322efa2c532a44a4
Ohio2 <sekmartyna4@gmail.com>
2020-11-07 22:15:58 +0100

    redid the script

--- repo/repo.sh
+++ repo/repo.sh
@@ -4,20 +4,13 @@ echo -e '\e[92mPinged GitHub.com'
 git pull 2>/dev/null
 echo -e '\e[94mRepo script.'
 echo -e '\e[39m1. Patch \e[91m(requires pkg.sh )\e[39m'
-for (( i=5; i>0; i--)); do
-  sleep 1 &
-  printf "$i \r"
-  wait
-done
-set input=1 #should be one
+read input
 if [ input=1 ] #should be one
 then 
  mkdir root_repo/ 2>/dev/null
- cd .. 
- cp * repo/root_repo 2>/dev/null
- cd repo/
- echo -e '\e[96mCopied!'
- cd root_repo/
+ cd ..
+ cp pkg.sh repo/root_repo
+ cd repo/root_repo/
 else
  echo -e '\e[91mError! Something broke! Check the script for more info, aditionally you could report this to my GitHub'
 fi
